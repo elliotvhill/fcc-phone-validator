f2b09515e5030e15b984ec849ea505f6c247f530
Elliot <ellthrowaway2@icloud.com>
2024-07-08 19:17:30 -0400

    more work on regex testing

--- script.js
+++ script.js
@@ -7,33 +7,36 @@ const numRegex = new RegExp("/(\d)?/g")
 const testNum = "+ 1(203) 123-4567";
 const testNum2 = "+1-2031234567";
 // const testNum3 = "1 555-555-5555"
-const testNum4 = "1 555 555 5555"
+// const testNum4 = "1 555 555 5555"
 const testNum3 = "15555555555"
-// const testNum4 = "555-5555"
+const testNum4 = "555-5555"
 // console.log(testNum.match(regex))
-const countryCodeRegex = /([\+\s]?)(1)?([\s\-]?)/g
-const areaCodeRegex = ""
-const sevenDigNumRegex = ""
 
 // const testRegEx = /(\+)?(\s)?([1])?([\(\-\s)]?)(\d)+([\)-\s)])(\d{4})/g
 // const testRegEx = /\+?(\s)?(1?)([\s\(\-)])/g
 
-// const testRegEx = /1\d{9}/g // just 10 digits √
-const testRegEx = /1[\s-]?\d{3}[\s-]?\d{3}[\s-]?\d{4}/g // 10 digits w/ " "'s OR "-"'s OR no spaces √
+const testRegEx = /1?[\s-]?\d{3}[\s-]?\d{3}[\s-]?\d{4}/g // 10 digits w/ " "'s OR "-"'s OR no spaces √
+
+const regexExclude = /[^\&\$\%\@]/g
+const testBadNum = "1$389%012340"
+// const testBadNum = "1389012340"
 
 
-// console.log(testNum, testNum.match(testRegEx))
+console.log(testNum, testNum.match(testRegEx))
 // console.log(testNum, testNum.match(countryCodeRegex))
-// console.log(testNum, testNum.replace("+", ""))
 // console.log(testNum2, testNum2.match(testRegEx))
 console.log(testNum3, testNum3.match(testRegEx))
 console.log(testNum4, testNum4.match(testRegEx))
+console.log(testBadNum, testBadNum.match(regexExclude))
+console.log(testBadNum.match(regexExclude) ? "bad chars" : "chars good")
 
 
 
 const checkInput = (e) => {
     if (!input.value) {
         alert("Please provide a phone number");
+    } else if (input.value.length < 10 || input.value.length > 16) {
+        alert("Please enter a valid US number")
     }
 };
 
