cbcd925ce46959e09a4401a10db074e94ed6d879
Elliot <ellthrowaway2@icloud.com>
2024-07-08 19:02:38 -0400

    making progress with regex testing

--- script.js
+++ script.js
@@ -6,30 +6,29 @@ const numRegex = new RegExp("/(\d)?/g")
 // const regex = /([+\s]?)?([1\s-\()]?)(\d)*?([-\s)])(\d)/g
 const testNum = "+ 1(203) 123-4567";
 const testNum2 = "+1-2031234567";
-const testNum3 = "1 555-555-5555"
-const testNum4 = "555-5555"
+// const testNum3 = "1 555-555-5555"
+const testNum4 = "1 555 555 5555"
+const testNum3 = "15555555555"
+// const testNum4 = "555-5555"
 // console.log(testNum.match(regex))
+const countryCodeRegex = /([\+\s]?)(1)?([\s\-]?)/g
+const areaCodeRegex = ""
+const sevenDigNumRegex = ""
 
-const testRegEx = /([\+\s]?)(1?)([\(-\s)]?)(\d{3})([\)-\s)])(\d{4})/g
+// const testRegEx = /(\+)?(\s)?([1])?([\(\-\s)]?)(\d)+([\)-\s)])(\d{4})/g
+// const testRegEx = /\+?(\s)?(1?)([\s\(\-)])/g
 
-console.log(testNum, testNum.match(testRegEx))
+// const testRegEx = /1\d{9}/g // just 10 digits √
+const testRegEx = /1[\s-]?\d{3}[\s-]?\d{3}[\s-]?\d{4}/g // 10 digits w/ " "'s OR "-"'s OR no spaces √
+
+
+// console.log(testNum, testNum.match(testRegEx))
+// console.log(testNum, testNum.match(countryCodeRegex))
 // console.log(testNum, testNum.replace("+", ""))
-console.log(testNum2, testNum2.match(testRegEx))
+// console.log(testNum2, testNum2.match(testRegEx))
 console.log(testNum3, testNum3.match(testRegEx))
 console.log(testNum4, testNum4.match(testRegEx))
 
-// +
-// " "
-// 1
-// " " | "-" | "(" | ""
-// 2
-// 2
-// 2
-// " " | "-" | ")" | ""
-// 4
-// 5
-// 8
-// 9
 
 
 const checkInput = (e) => {
