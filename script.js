6def2c649cbb06fe05ccb9f1f08c86c7b03dab2b
Elliot <ellthrowaway2@icloud.com>
2024-07-08 16:41:18 -0400

    building test regexs

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
 const input = document.getElementById("user-input");
 const checkBtn = document.getElementById("check-btn");
 const clearBtn = document.getElementById("clear-btn");
-const results = document.getElementById("results-div")
+const results = document.getElementById("results-div");
+const numRegex = new RegExp("/(\d)?/g")
+// const regex = /([+\s]?)?([1\s-\()]?)(\d)*?([-\s)])(\d)/g
+const testNum = "+ 1(203) 123-4567";
+const testNum2 = "+1-2031234567";
+const testNum3 = "1 555-555-5555"
+const testNum4 = "555-5555"
+// console.log(testNum.match(regex))
+
+const testRegEx = /([+\s]?)(1?)([\(-\s\d]{3})/g
+
+console.log(testNum, testNum.match(testRegEx))
+console.log(testNum, testNum.replace("+", ""))
+console.log(testNum2, testNum2.match(testRegEx))
+console.log(testNum3, testNum3.match(testRegEx))
+console.log(testNum4, testNum4.match(testRegEx))
+
+// +
+// " "
+// 1
+// " " | "-" | "(" | ""
+// 2
+// 2
+// 2
+// " " | "-" | ")" | ""
+// 4
+// 5
+// 8
+// 9
+
 
 const checkInput = (e) => {
     if (!input.value) {
