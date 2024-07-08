15f506cbecdba5179dfae3a4062ec700035d4a93
Elliot <ellthrowaway2@icloud.com>
2024-07-08 19:36:06 -0400

    boundary tweaked, successful results message

--- script.js
+++ script.js
@@ -7,15 +7,16 @@ const numRegex = new RegExp("/(\d)?/g")
 const testNum = "+ 1(203) 123-4567";
 const testNum2 = "+1-2031234567";
 // const testNum3 = "1 555-555-5555"
-// const testNum4 = "1 555 555 5555"
-const testNum3 = "15555555555"
-const testNum4 = "555-5555"
+// const testNum3 = "1-555-555-5555" // √
+// const testNum4 = "1 555 555 5555" // √
+// const testNum3 = "15555555555" // √
+// const testNum4 = "555-5555" // √
 // console.log(testNum.match(regex))
 
 // const testRegEx = /(\+)?(\s)?([1])?([\(\-\s)]?)(\d)+([\)-\s)])(\d{4})/g
 // const testRegEx = /\+?(\s)?(1?)([\s\(\-)])/g
 
-const testRegEx = /1?[\s-]?\d{3}[\s-]?\d{3}[\s-]?\d{4}/g // 10 digits w/ " "'s OR "-"'s OR no spaces √
+const testRegEx = /1?(^[\s-])?\d{3}[\s-]?\d{3}[\s-]?\d{4}/g // 10 digits w/ " "'s OR "-"'s OR no spaces √
 
 const regexExclude = /[^\&\$\%\@]/g
 const testBadNum = "1$389%012340"
@@ -23,10 +24,9 @@ const testBadNum = "1$389%012340"
 
 
 console.log(testNum, testNum.match(testRegEx))
-// console.log(testNum, testNum.match(countryCodeRegex))
-// console.log(testNum2, testNum2.match(testRegEx))
-console.log(testNum3, testNum3.match(testRegEx))
-console.log(testNum4, testNum4.match(testRegEx))
+console.log(testNum2, testNum2.match(testRegEx))
+// console.log(testNum3, testNum3.match(testRegEx))
+// console.log(testNum4, testNum4.match(testRegEx))
 // console.log(testBadNum, testBadNum.match(regexExclude))
 // console.log(testBadNum.match(regexExclude) ? "bad chars" : "chars good")
 
@@ -35,9 +35,11 @@ console.log(testNum4, testNum4.match(testRegEx))
 const checkInput = (e) => {
     if (!input.value) {
         alert("Please provide a phone number");
-    } else if (input.value.length < 10 || input.value.length > 16 || input.value.match(testRegEx === null)) {
+    } else if (input.value.length < 10 || input.value.length > 16 || !input.value.match(testRegEx)) {
         alert("Please enter a valid US number")
         console.log("invalid number")
+    } else {
+        results.innerText = `${input.value} is a valid US phone number`
     }
 };
 
