751b4ba57c59eb3e01f4383d5096f8159cb4ed60
Elliot <ellthrowaway2@icloud.com>
2024-07-10 19:32:07 -0400

    cleaned up some unused test code, more regex testing

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const checkBtn = document.getElementById("check-btn");
 const clearBtn = document.getElementById("clear-btn");
 const results = document.getElementById("results-div");
 const numRegex = new RegExp("/(\d)?/g")
-// const regex = /([+\s]?)?([1\s-\()]?)(\d)*?([-\s)])(\d)/g
+const regex = /(\+)?(\s)?([1\s])?([\s\(\-\s)]?)(\d)+([\)-\s)])(\d{4})/g
 const testNum = "+ 1(203) 123-4567";
 const testNum2 = "+1-2031234567"; // √
 // const testNum3 = "1 555-555-5555"
@@ -13,20 +13,16 @@ const testNum2 = "+1-2031234567"; // √
 // const testNum4 = "555-5555" // √
 // console.log(testNum.match(regex))
 
-// const testRegEx = /(\+)?(\s)?([1])?([\(\-\s)]?)(\d)+([\)-\s)])(\d{4})/g
-// const testRegEx = /\+?(\s)?(1?)([\s\(\-)])/g
 
-const testRegEx = /(^\+)?(\s)?1?(^[\s-\(])?(\d{3}[\s-\)])?\d{3}[\s-]?\d{4}/g // 10 digits w/ " "'s OR "-"'s OR no spaces OR "(" OR ")" √
+// const testRegEx = /(^\+)?(\s)?1?(^[\s-\(])?(\d{3}[\s-\)])?\d{3}[\s-]?\d{4}/g // 10 digits w/ " "'s OR "-"'s OR no spaces OR "(" OR ")" √
 
 const regexExclude = /[^\&\$\%\@]/g
 const testBadNum = "1$389%012340"
 // const testBadNum = "1389012340"
 
 
-console.log(testNum, testNum.match(testRegEx))
-console.log(testNum2, testNum2.match(testRegEx))
-// console.log(testNum3, testNum3.match(testRegEx))
-// console.log(testNum4, testNum4.match(testRegEx))
+console.log(testNum, testNum.match(regex))
+console.log(testNum2, testNum2.match(regex))
 // console.log(testBadNum, testBadNum.match(regexExclude))
 // console.log(testBadNum.match(regexExclude) ? "bad chars" : "chars good")
 
@@ -35,7 +31,7 @@ console.log(testNum2, testNum2.match(testRegEx))
 const checkInput = (e) => {
     if (!input.value) {
         alert("Please provide a phone number");
-    } else if (input.value.length < 10 || input.value.length > 16 || !input.value.match(testRegEx)) {
+    } else if (input.value.length < 10 || input.value.length > 16 || !input.value.match(regex)) {
         results.innerText = `Invalid US number: ${input.value}`
         console.log("invalid number")
     } else {
