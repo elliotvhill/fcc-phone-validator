a963b2a26a351882f57b1e7c4007c14101b195a0
Elliot <ellthrowaway2@icloud.com>
2024-07-11 15:07:34 -0400

    getting closer with regex, removed unused test code

--- script.js
+++ script.js
@@ -2,32 +2,21 @@ const input = document.getElementById("user-input");
 const checkBtn = document.getElementById("check-btn");
 const clearBtn = document.getElementById("clear-btn");
 const results = document.getElementById("results-div");
-const numRegex = new RegExp("/(\d)?/g")
+// const numRegex = new RegExp("/(\d)?/g")
 const regex = /(\+)?(\s)?([1\s])?([\s\(\-\s)])?(\d{3})([\)\-\s)][\s])?(\d{4})/g
-const testNum = "+ 1(203) 123-4567";
-const testNum2 = "+1-2031234567"; // √
 
 
-// const testRegEx = /(\+?)(1+)([\s-])?([^\&\$\%\@\#\.])(\(\d{3}\))*(\d{3})([\s-])?(\d{4})[^\&\$\%\@\#\.]/g
-const testRegEx = /(\+?1)?([\s-\(]?\s?)(\d{3})([\s-\)]?\s?)(\d{4})/g
+const testRegEx = /\+?1*[\s\-]?\(?\d{3}\)\4?[\s\-]?\d{3}[\s\-]?\d{4}/g // closest so far
 const areaCodes = /(\(\d{3}\))/g // correctly matches "(" and ")" -- both needed
 
 const regexExclude = /[^\&\$\%\@.]/g
 const testBadNum = "1$389%012340"
 
 
-// console.log(testNum, testNum.match(regex))
-// console.log(testNum, testNum.match(testRegEx))
-// console.log(testBadNum, testBadNum.match(regexExclude))
-// console.log(testBadNum.match(regexExclude) ? "bad chars" : "chars good")
-
-
-
 const checkInput = (e) => {
     if (!input.value) {
         alert("Please provide a phone number");
     } else if (input.value.length < 10 || input.value.length > 16 || !input.value.match(testRegEx)) {
-    // } else if (input.value.length < 10 || input.value.length > 16 || !input.value.match(regex)) {
         results.innerText = `Invalid US number: ${input.value}`
         console.log("invalid number:", input.value)
     } else {
