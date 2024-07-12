ec142459dc09902ca7ebc9f6cad4c2cb58902a6e
Elliot <ellthrowaway2@icloud.com>
2024-07-12 16:04:09 -0400

    regex matches + only at beginning of string

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ const results = document.getElementById("results-div");
 // const numRegex = new RegExp("/(\d)?/g")
 // const regex = /(?<=[\+\s1])([\s\-]?)(\(\d{3}\)|\d{3})([\s\-]?)(\d{3})([\s\-]?)(\d{4})/g
 
-
 // 5555555555 // still not working
 // 555-555-5555 // still not working
+// (555)555-5555 // still not working
 
 
 // closest so far:
-const optPlus = /(\+?)(\s?)\b1([\s\-]?)(\(\d{3}\)|\d{3})([\s\-]?)(\d{3})([\s\-]?)(\d{4})/g // working- accepts only US CC, + or no +, +/- parentheses
+const optPlus = /(^\+?)(\s?)\b1([\s\-]?)(\(\d{3}\)|\d{3})([\s\-]?)(\d{3})([\s\-]?)(\d{4})/g // working- accepts only US CC, strictly + or no +, +/- parentheses
 
 
 
