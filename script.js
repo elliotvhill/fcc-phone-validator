c974649a79ecb614c2ce5401e508897dea29dcc2
Elliot <ellthrowaway2@icloud.com>
2024-07-12 17:39:11 -0400

    app functions as intended, all user stories met

--- script.js
+++ script.js
@@ -2,24 +2,12 @@ const input = document.getElementById("user-input");
 const checkBtn = document.getElementById("check-btn");
 const clearBtn = document.getElementById("clear-btn");
 const results = document.getElementById("results-div");
-// const numRegex = new RegExp("/(\d)?/g")
-// const regex = /(?<=[\+\s1])([\s\-]?)(\(\d{3}\)|\d{3})([\s\-]?)(\d{3})([\s\-]?)(\d{4})/g
-
-// 5555555555 // still not working
-// 555-555-5555 // still not working
-// (555)555-5555 // still not working
-
-
-// closest so far:
-const optPlus = /(^\+?)(\s?)\b1([\s\-]?)(\(\d{3}\)|\d{3})([\s\-]?)(\d{3})([\s\-]?)(\d{4})/g // working- accepts only US CC, strictly + or no +, +/- parentheses
-
-
+const regex = /^(\+?\b1)?([\s\-])?(\(\d{3}\)|\d{3})([\s\-]?)(\d{3})([\s\-])?(\d{4}$)/g
 
 const checkInput = () => {
     if (!input.value) {
         alert("Please provide a phone number");
-    // } else if (input.value.length >= 10 && input.value.length <= 16 && input.value.match(regex)) {
-    } else if (input.value.length >= 10 && input.value.length <= 18 && input.value.match(optPlus)) {
+    } else if (input.value.length >= 10 && input.value.length <= 18 && input.value.match(regex)) {
         results.innerText = `Valid US number: ${input.value}`
         console.log("Valid number:", input.value)
     } else {
