f18f60f878fe71f7d1cfc36fa83d7efd3e3e112e
Elliot <ellthrowaway2@icloud.com>
2024-07-12 17:40:51 -0400

    cleaned up formatting for clarity

--- script.js
+++ script.js
@@ -2,24 +2,29 @@ const input = document.getElementById("user-input");
 const checkBtn = document.getElementById("check-btn");
 const clearBtn = document.getElementById("clear-btn");
 const results = document.getElementById("results-div");
-const regex = /^(\+?\b1)?([\s\-])?(\(\d{3}\)|\d{3})([\s\-]?)(\d{3})([\s\-])?(\d{4}$)/g
+const regex =
+    /^(\+?\b1)?([\s\-])?(\(\d{3}\)|\d{3})([\s\-]?)(\d{3})([\s\-])?(\d{4}$)/g;
 
 const checkInput = () => {
     if (!input.value) {
         alert("Please provide a phone number");
-    } else if (input.value.length >= 10 && input.value.length <= 18 && input.value.match(regex)) {
-        results.innerText = `Valid US number: ${input.value}`
-        console.log("Valid number:", input.value)
+    } else if (
+        input.value.length >= 10 &&
+        input.value.length <= 18 &&
+        input.value.match(regex)
+    ) {
+        results.innerText = `Valid US number: ${input.value}`;
+        console.log("Valid number:", input.value);
     } else {
-        results.innerText = `Invalid US number: ${input.value}`
-        console.log(`Invalid US number: ${input.value}`)
+        results.innerText = `Invalid US number: ${input.value}`;
+        console.log(`Invalid US number: ${input.value}`);
     }
 };
 
 const clear = () => {
-    input.value = ""
-    results.textContent = ""
-}
+    input.value = "";
+    results.textContent = "";
+};
 
 checkBtn.addEventListener("click", checkInput);
 clearBtn.addEventListener("click", clear);
