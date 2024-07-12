1e41a042a7e92df6917681d8265c1862d1821d77
Elliot <ellthrowaway2@icloud.com>
2024-07-12 11:42:51 -0400

    regex accepts valid optional chars, requires both parentheses if included; country code not passing though

--- script.js
+++ script.js
@@ -3,23 +3,17 @@ const checkBtn = document.getElementById("check-btn");
 const clearBtn = document.getElementById("clear-btn");
 const results = document.getElementById("results-div");
 // const numRegex = new RegExp("/(\d)?/g")
-const regex = /(\+)?(\s)?([1\s])?([\s\(\-\s)])?(\d{3})([\)\-\s)][\s])?(\d{4})/g
+const regex = /(?<=[\+\s1])([\s\-]?)(\(\d{3}\)|\d{3})([\s\-]?)(\d{3})([\s\-]?)(\d{4})/g
 
 // 12345678900
 
-// "1" CC required, rejects other CCs
-const simpleDigitsRegex = /(?<=[\+\s1])([\s\-]?)(\d{3})([\s\-]?)(\d{3})([\s\-]?)(\d{4})/g // closest so far
-
-const areaCodes = /(\(\d{3}\))/g // correctly matches "(" and ")" -- both needed
-
-const regexExclude = /[^\&\$\%\@.]/g
-const testBadNum = "1$389%012340"
+// const simpleDigitsRegex = /(?<=[\+\s1])([\s\-]?)(\(\d{3}\)|\d{3})([\s\-]?)(\d{3})([\s\-]?)(\d{4})/g // closest so far
 
 
 const checkInput = (e) => {
     if (!input.value) {
         alert("Please provide a phone number");
-    } else if (input.value.length >= 10 && input.value.length <= 16 && input.value.match(simpleDigitsRegex)) {
+    } else if (input.value.length >= 10 && input.value.length <= 16 && input.value.match(regex)) {
         results.innerText = `Valid US number: ${input.value}`
         console.log("Valid number:", input.value)
     } else {
